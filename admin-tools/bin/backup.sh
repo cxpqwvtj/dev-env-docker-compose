8625c61f57c0fe3977c77ae9b37df9dd29c7d0f3
cxpqwvtj <cxpqwvtj@gmail.com>
2016-06-29 07:05:22 +0900

    バックアップフォルダにバックアップしたファイルを保存するように修正

--- admin-tools/bin/backup.sh
+++ admin-tools/bin/backup.sh
@@ -16,6 +16,12 @@ abs_dirname() {
 
 script_dir="$(abs_dirname "$0")"
 file_name_date="`date '+%Y%m%d%H%M%S'`"
+backup_dir="${script_dir}/../../backup"
 
-tar zcvf "${file_name_date}-ldap.tar.gz" -C "$script_dir/../../ldap" data
-tar zcvf "${file_name_date}-portal.tar.gz" -C "$script_dir/../../project" data
+if [ ! -e "${backup_dir}" ]; then
+  echo "create backup dir"
+  mkdir -p "${backup_dir}"
+fi
+
+tar zcvf "${backup_dir}/${file_name_date}-ldap.tar.gz" -C "$script_dir/../../ldap" data
+tar zcvf "${backup_dir}/${file_name_date}-portal.tar.gz" -C "$script_dir/../../project" data
