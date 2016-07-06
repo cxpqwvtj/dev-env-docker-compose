b957b9e19bbc03989333313a8c612dfcf22705b2
cxpqwvtj <cxpqwvtj@gmail.com>
2016-07-06 22:28:44 +0900

    ドメイン切り替えスクリプト作成

---
+++ admin-tools/bin/replace-domain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ../env
+
+files=(
+  ../../ldap/docker-compose.yml
+  ../../proxy/docker-compose.yml
+  ../../project/docker-compose.yml
+)
+
+for file in ${files[@]}; do
+  OS_NAME=`uname -s`
+  if [ $OS_NAME = 'Darwin' ]; then
+    FILE_EXT="''"
+  fi
+  COMMAND="sed -i ${FILE_EXT} -e \"s/localhost/${HOST_NAME}/g\" ${file}"
+  echo $COMMAND
+  eval ${COMMAND}
+done
+
+
+
