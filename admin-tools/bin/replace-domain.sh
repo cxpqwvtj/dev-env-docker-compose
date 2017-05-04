2fad858944e38733bba4d5a0156fdb94e940b8c6
cxpqwvtj <cxpqwvtj@gmail.com>
2017-05-04 10:34:43 +0900

    localhostではなくドメインを定義するように変更

--- admin-tools/bin/replace-domain.sh
+++ admin-tools/bin/replace-domain.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+pushd $(dirname ${BASH_SOURCE:-$0})
+
 source ../env
 
 files=(
   ../../ldap/docker-compose.yml
   ../../nginx/docker-compose.yml
   ../../project/docker-compose.yml
+  ../../nginx/config/conf.d/account.conf
+  ../../nginx/config/conf.d/chat.conf
+  ../../nginx/config/conf.d/default.conf
+  ../../nginx/config/conf.d/gitlab.conf
+  ../../nginx/config/conf.d/jenkins.conf
+  ../../nginx/config/conf.d/owncloud.conf
+  ../../nginx/config/conf.d/redmine.conf
 )
 
 for file in ${files[@]}; do
@@ -13,10 +22,9 @@ for file in ${files[@]}; do
   if [ $OS_NAME = 'Darwin' ]; then
     FILE_EXT="''"
   fi
-  COMMAND="sed -i ${FILE_EXT} -e \"s/localhost/${HOST_NAME}/g\" ${file}"
+  COMMAND="sed -i ${FILE_EXT} -e \"s/example.com/${HOST_NAME}/g\" ${file}"
   echo $COMMAND
   eval ${COMMAND}
 done
 
-
-
+popd
