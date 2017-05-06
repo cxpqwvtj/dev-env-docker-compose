d937f9aed7ffa0d16fd568ffbba49a97e948f310
cxpqwvtj <cxpqwvtj@gmail.com>
2017-05-06 19:05:46 +0900

    443ポートに置換するシェルの作成

--- admin-tools/setup/enable-ssl.sh
+++ admin-tools/setup/enable-ssl.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 pushd $(dirname ${BASH_SOURCE:-$0})
+
+OS_NAME=`uname -s`
+if [ $OS_NAME = 'Darwin' ]; then
+  FILE_EXT="''"
+fi
+
+for file in `\find ../../nginx/nginx/config/conf.d -maxdepth 1 -type f -name "*.conf"`; do
+  eval "sed -i $FILE_EXT -e \"s/listen 80/listen 443/g\" $file"
+done
 
 cp -f ../../nginx/nginx/config/conf.d/default.conf.ssl.example ../../nginx/nginx/config/conf.d/default.conf
